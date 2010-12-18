525f1699ea2392276f099edcbd76e31cfe825808
Arafat Mohamed <github@arafatm.com>
2010-12-18 16:08:25 -0600

    s/context/cx/g in fable.js

--- public/javascripts/fable.js
+++ public/javascripts/fable.js
@@ -5,38 +5,39 @@
 
     this.use('Template');
 
-    this.get('#/', function(context) {
-      context.redirect('#/projects');
+    this.get('#/', function(cx) {
+      cx.redirect('#/projects');
     });
 
-    this.get('#/projects', function(context) {
-      context.render('templates/project_new.template').swap(context.$element());
+    this.get('#/projects', function(cx) {
+      cx.render('templates/project_new.template').swap(cx.$element());
       this.load('/data/projects.json')
       .then(function(projects) {
         $.each(projects, function(i, project) {
-          context.render('templates/projects.template', {project: project})
-            .appendTo(context.$element());
+          cx.render('templates/projects.template', {project: project})
+            .appendTo(cx.$element());
         });
       })
-      .then(context.trigger('testalert', 1));
+      .then(cx.trigger('testalert', 1));
     });
 
     // GET new
     this.get('#/projects/new', function(cx) {
-      context.app.swap('');
-      context.render('templates/project_form.template')
-        .swap(context.$element());
+      cx.log('new');
+      cx.app.swap('');
+      cx.render('templates/project_form.template')
+        .swap(cx.$element());
     });
 
-    this.get('#/project/:id', function(context) {
+    this.get('#/project/:id', function(cx) {
       this.load("data/"+this.params['id']+".json")
       .then(function(project) {
-        context.render('templates/project.template', {project: project})
-          .swap(context.$element());
+        cx.render('templates/project.template', {project: project})
+          .swap(cx.$element());
       })
       .then(function(project) {
         $.each(project.features, function(i, feature) {
-          context.render('templates/feature.template', {feature: feature})
+          cx.render('templates/feature.template', {feature: feature})
           .appendTo('.feature');
         });
       });
